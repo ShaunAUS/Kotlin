0af996ca1a21b78ac0f57217a9e85072d95cdb34
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-03-27 19:58:06 +0900

    v7
    지영초기화 / overriding

---
+++ InflearnV7/SpringDataJPA/data-jpa practice/src/main/java/study/datajpa/controller/HelloController.java
@@ -0,0 +1,11 @@
+package study.datajpa.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+
+
+}
---
+++ InflearnV7/SpringDataJPA/data-jpa practice/src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package study.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
