ac49169695211900d88a128b4a0573b7093a3a66
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-03-29 10:31:58 +0900

    v9
    추상클래스/인터페이스 추가

---
+++ KotlinV9/src/main/kotlin/pratice/Abstract.kt
@@ -0,0 +1,31 @@
+package pratice
+
+fun main() {
+
+    var obj1 = testClass4()
+
+    var obj2 = testClass4()
+    testObject(obj2)
+}
+
+open  abstract class testClass{
+
+    fun testsMethod1(){
+        print("testMethod 1")
+    }
+
+    open abstract fun testsMethod2()
+
+}
+
+class testClass4 : testClass(){
+
+    override fun testsMethod2() {
+        print("testclass4 의 testMethod2")
+    }
+}
+
+fun testObject(a1:testClass){
+    a1.testsMethod1()
+    a1.testsMethod2()
+}
---
+++ KotlinV9/src/main/kotlin/pratice/Property.kt
@@ -0,0 +1,52 @@
+package pratice
+
+fun main() {
+
+
+    var obj1 = PropertyTestClass(100,200)
+
+    println(obj1.a1)
+    println(obj1.a2)
+
+    obj1.a1 = 2000
+
+    var obj2 = PropertyTestClass2()
+    obj2.v3 = 5000   //setter
+    println(obj2.v3) //getter
+
+}
+
+
+//기본 생성자로 생성시 ->  getter/setter/멤버변수 설정 자동
+//기본 생성자 생략 가능
+class PropertyTestClass (var a1:Int , val a2:Int){
+
+
+    //var or val 은 주 생성자한테만 붙일수 있다.
+    //constructor(var a1:Int,var a2:Int,var a3:Int) : this(a1,a2){}
+}
+
+
+//멤버변수 선언 -> getter/setter 설정 자동
+class PropertyTestClass2{
+    var v1 = 0
+    val v2 = 30
+
+    get(){
+        println("v2 getter")
+        return field
+    }
+
+
+    var v3 = 100
+
+        //코틀린은 게터,세터 자동생성이기떄문에 직접작성시 이렇게 해야함
+        get(){
+            println("get메서드 호출")
+            return field   //field 는 v3를 의미함
+        }
+        set(value){
+            println("set메서드 호출")
+            field = value // field 는 v3를 의미함
+        }
+}
